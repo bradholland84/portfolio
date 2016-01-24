423c04e230c9586f1d831478edbc4542b736020f
bradholland84 <bholland.mail@gmail.com>
2016-01-23 22:50:47 -0800

    bootstrap up and running

--- app.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Brad on 1/23/2016.
- */
-$(function() {
-    'use strict';
-
-
-
-});
---
+++ js/app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Brad on 1/23/2016.
+ */
+$(function() {
+    'use strict';
+
+    $('body').flowtype({
+        minimum   : 500,
+        maximum   : 1200,
+        minFont   : 12,
+        maxFont   : 40,
+        fontRatio : 30
+    });
+
+
+});
