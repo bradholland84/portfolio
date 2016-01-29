19e62abbaffca4a0318892bd5e09ab1244496304
bradholland84 <bholland.mail@gmail.com>
2016-01-28 19:21:38 -0800

    began work on project contents and their hover effects

--- js/app.js
+++ js/app.js
@@ -4,10 +4,17 @@
 $(function() {
     'use strict';
     window.sr = ScrollReveal({
-        origin: 'left',
         duration: 500,
-        rotate: { x : 0, y : 0, z : -90 },
         mobile: true
+    });
+
+    sr.reveal('.sidebar-link', {
+        origin: 'left',
+        rotate: { x : 0, y : 0, z : -90 }
+        });
+
+    sr.reveal('.content-container', {
+        origin: 'bottom'
     });
 
     $('body').flowtype({
@@ -17,21 +24,5 @@ $(function() {
         maxFont   : 40,
         fontRatio : 30
     });
-
-    var options = {
-        scrollSource: document,
-        scrollbar: '.scrollbar',
-        dragSource: document,
-        horizontal: false,
-        itemNav: 'forceCentered',
-        startAt: 0,
-        speed: 300,
-        mouseDragging: 1,
-        touchDragging: 1
-    };
-    $('.frame').sly(options);
-
-    sr.reveal('.sidebar-link');
-
 
 });
